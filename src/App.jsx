2a0c97934460f4b02af24f1e8969b57472e32464
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-07-31 12:59:24 +0100

    frotnef

--- src/App.jsx
+++ src/App.jsx
@@ -13,33 +13,39 @@ import EditBook from "./components/EditBook";
 import DeleteBook from "./components/DeleteBook";
 
 function App() {
-  const [role, setRole] = useState('')
+  const [role, setRole] = useState('');
 
   axios.defaults.withCredentials = true;
+
   useEffect(() => {
     axios.get('https://e-assignment-platform-backend.onrender.com/auth/verify')
-    .then(res => {
-      if(res.data.login) {
-        setRole(res.data.role)
-      } else {
-       setRole('') 
-      }
-      console.log(res)
-    }).catch(err => console.log(err))
-  }, [])
+      .then(res => {
+        if (res.data.login) {
+          setRole(res.data.role);
+        } else {
+          setRole('');
+        }
+        console.log('Verify response:', res);
+      })
+      .catch(err => {
+        console.error('Verify request error:', err);
+        setRole(''); // Ensure role is reset on error
+      });
+  }, []);
+
   return (
     <BrowserRouter>
-      <Navbar role = {role}/>
+      <Navbar role={role} />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/books" element={<Books role = {role}/>}></Route>
-        <Route path="/login" element={<Login setRoleVar = {setRole}/>}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/addstudent" element={<AddStudent />}></Route>
-        <Route path="/logout" element={<Logout setRole = {setRole}/>}></Route>
-        <Route path="/addbook" element={<AddBook />}></Route>
-        <Route path="/book/:id" element={<EditBook />}></Route>
-        <Route path="/delete/:id" element={<DeleteBook />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books role={role} />} />
+        <Route path="/login" element={<Login setRoleVar={setRole} />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addstudent" element={<AddStudent />} />
+        <Route path="/logout" element={<Logout setRole={setRole} />} />
+        <Route path="/addbook" element={<AddBook />} />
+        <Route path="/book/:id" element={<EditBook />} />
+        <Route path="/delete/:id" element={<DeleteBook />} />
       </Routes>
     </BrowserRouter>
   );
