b2402f24293853e7491f86890fcbd9d0c3003f63
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-07-31 14:08:28 +0100

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -16,27 +16,29 @@ function App() {
   const [role, setRole] = useState('')
 
   axios.defaults.withCredentials = true;
+  
   useEffect(() => {
-    axios.get('http://localhost:3001/auth/verify')
+    axios.get('https://e-assignment-platform-backend.onrender.com/auth/verify')
     .then(res => {
       if(res.data.login) {
         setRole(res.data.role)
       } else {
-       setRole('') 
+        setRole('') 
       }
       console.log(res)
     }).catch(err => console.log(err))
   }, [])
+
   return (
     <BrowserRouter>
-      <Navbar role = {role}/>
+      <Navbar role={role}/>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/books" element={<Books role = {role}/>}></Route>
-        <Route path="/login" element={<Login setRoleVar = {setRole}/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/books" element={<Books role={role} />}></Route>
+        <Route path="/login" element={<Login setRoleVar={setRole} />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/addstudent" element={<AddStudent />}></Route>
-        <Route path="/logout" element={<Logout setRole = {setRole}/>}></Route>
+        <Route path="/logout" element={<Logout setRole={setRole} />}></Route>
         <Route path="/addbook" element={<AddBook />}></Route>
         <Route path="/book/:id" element={<EditBook />}></Route>
         <Route path="/delete/:id" element={<DeleteBook />}></Route>
